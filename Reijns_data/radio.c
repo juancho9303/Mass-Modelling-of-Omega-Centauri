f3d995c1ba466b7c246ed12e6fd34405e4b6d28f
juancho9303 <juancho9303@gmail.com>
2016-02-09 21:55:23 -0500

    added new database analysis

--- Reijns_data/radio.c
+++ Reijns_data/radio.c
@@ -10,7 +10,7 @@ int main (void)
 
   double a[N],b[N],c[N],d[N],e[N],f[N],g[N],h[N],j[N],k[N],l[N],AR[N],DEC[N],AR_c[N],DEC_c[N],dis[N];
   
-  FILE *data, *center, *radius;
+  FILE *data, *center, *radius, *script;
   data = fopen("table4.dat", "r");
   center = fopen("center.dat", "r");
   radius = fopen("distancia.dat" , "w");
@@ -26,10 +26,16 @@ int main (void)
 	  fprintf(radius,"%lf\t %lf\t %lf\n", dis[i], j[i], k[i]);
 	}      
     }
-  
+    
   fclose(data);
   fclose(center);
   fclose(radius); 
+  
+  script = fopen( "script.gpl", "w" );
+  fprintf(script, "set grid\nset terminal png\nset output 'velocity_vs_rad.png'\nset nokey\n");
+  fprintf( script, "set title 'Velocity vs Radius'\n" );
+  fprintf( script, "plot 'distancia.dat' u 1:2\n");
+  fclose(script);
   
   //  printf("E. Cinetica: %Lf\t E. Potencial: %Lf\t E. Total: %Lf\n", kin_tot, pot_tot, tot_energy);
   
